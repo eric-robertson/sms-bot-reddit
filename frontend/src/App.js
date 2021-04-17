f348155900052d08c7148843ea7894325eb7f454
Asher Gilani <gilaniasher@gmail.com>
2021-04-17 16:41:11 +0100

    Started adding tab buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [lookupTab, setLookupTab] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setLookupTab(true)}>
+        Lookup Phone Rules
+      </button>
+      <button onClick={() => setLookupTab(false)}>
+        Add Phone Rules
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
